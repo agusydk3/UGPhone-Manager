ab3672bcdb6d87b90cc35cd3f1be42633e5a1137
AgusXD <57493908+agusydk3@users.noreply.github.com>
2025-08-11 00:13:06 +0700

    Update tampermonkey.user.js

--- tampermonkey/tampermonkey.user.js
+++ tampermonkey/tampermonkey.user.js
@@ -7,6 +7,7 @@
 // @require      https://raw.githubusercontent.com/agusydk3/UGPhone-Manager/main/tampermonkey/ugphone.js
 // @match        https://www.ugphone.com/*
 // @match        https://*.ugphone.com/*
+// @icon         https://raw.githubusercontent.com/agusydk3/UGPhone-Manager/main/img/miku.jpeg
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setValue
 // @grant        GM_getValue
