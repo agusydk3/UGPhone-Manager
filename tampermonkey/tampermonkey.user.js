0c60950afb8cfaf631f5700994fc27965c6bd4df
AgusXD <57493908+agusydk3@users.noreply.github.com>
2025-08-10 23:50:47 +0700

    Create tampermonkey.user.js

---
+++ tampermonkey/tampermonkey.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         UGPhone Login
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Auto login / login helper untuk UGPhone
+// @author       AgusXD
+// @require      https://raw.githubusercontent.com/agusydk3/UGPhone-Manager/main/tampermonkey/ugphone.js
+// @match        https://www.ugphone.com/*
+// @match        https://*.ugphone.com/*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addStyle
+// @grant        unsafeWindow
+// @run-at       document-end
+// ==/UserScript==
